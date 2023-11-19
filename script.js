1458832870686a9a6f7cfb68ea390ceb17705cae
Suraj <77872306+DrSuraj@users.noreply.github.com>
2023-11-19 18:27:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+      const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+      return isLeapYear ? 366 : 365;
+    }
+
+    function calculateDays() {
+      const yearInput = document.getElementById('yearInput').value;
+      const resultElement = document.getElementById('result');
+
+      if (!isNaN(yearInput) && yearInput >= 1 && yearInput <= 9999) {
+        const days = daysOfAYear(parseInt(yearInput));
+        resultElement.textContent = `The year ${yearInput} has ${days} days.`;
+      } else {
+        resultElement.textContent = 'Please enter a valid year.';
+      }
+    }
